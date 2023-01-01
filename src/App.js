4db343a18035e33afd4b5288aea77b81e06a333c
cartwright <amaechiisaac450@gmail.com>
2023-01-01 15:27:46 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import logo from './Cartwright_Promotions_logo.png';
+import logo1 from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="Https://www.facebook.com/cartwrightpromotions"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cartwright Promotions
         </a>
+        <br/>
+        <img src={logo1} className= "App-logo" alt="preloader" />
       </header>
     </div>
   );
